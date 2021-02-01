47d69bfc4da93a32f6f71ee1bc6957f204bd1c42
Driijko <driijko@gmail.com>
2021-02-01 18:12:51 -0500

    Add InteractiveElement component to Element Tools folder

---
+++ src/App/Content/2 Elements/0 Element Tools/InteractiveElement.js
@@ -0,0 +1,84 @@
+// IMPORTS ////////////////////////////////////////////////////////////
+import {useState, useEffect, Children, cloneElement} from "react";
+
+// COMPONENT //////////////////////////////////////////////////////////
+function InteractiveElement({children, focus, enterSelect}) {
+
+    // HIGHLIGHTING ////////////////////////////////////////////////////////
+    const [highlight, setHighlight] = useState(null);
+
+    // Focus -----------------------------------------------------------
+    useEffect(()=> {
+        if (focus) {
+            setHighlight(true);
+        }
+        else if (highlight) {
+            setHighlight(false);
+        };
+    },[focus]);
+
+    // Mouse Handlers --------------------------------------------------
+    function handleMouseEnter() {
+        setHighlight(true);
+    };
+
+    function handleMouseLeave() {
+        setHighlight(false);
+    };
+
+    // SELECTION ////////////////////////////////////////////////////////////
+    const [selected, setSelected] = useState(false);
+
+    // Mouse Handler ------------------------------------------
+    function handleClick() {
+        setSelected(true);
+    };
+
+    // User presses 'enter' key --------------------------------
+    useEffect(()=> {
+        if (enterSelect) {
+            setSelected(true);
+        };
+    }, [enterSelect]);
+
+    // INTERACTIVITY /////////////////////////////////////////////////////
+    const [interactivity, setInteractivity] = useState("able");
+
+    useEffect(()=> {
+        if (interactivity !== "dormant") {
+            if (selected) {
+                setInteractivity("selected");
+            }
+            else if (highlight) {
+                setInteractivity("highlight");
+            }
+            else {
+                setInteractivity("able");              
+            };
+        }
+    }, [highlight, selected]);
+
+    const modifiedChildren = Children.map(children, (child, index)=> {
+        if (index === 0) {
+            return cloneElement(child, {
+                interactivity: interactivity,
+                onClick: handleClick,
+                onMouseEnter: handleMouseEnter,
+                onMouseLeave: handleMouseLeave,
+            });
+        }
+        else {
+            return cloneElement(child, {
+                interactivity: interactivity,
+            });
+        };
+    });
+
+
+    // RENDER //////////////////////////////////////////////////////////////
+    return (
+        modifiedChildren
+    );
+};
+
+export default InteractiveElement;
